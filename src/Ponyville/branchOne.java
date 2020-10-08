bdb58093f408b668ef1b3b25665ed2ab965bd5a1
Bokun-Zhao <Sam2565876330@outlook.com>
2020-10-07 23:00:05 -0400

    new feature

--- src/Ponyville/branchOne.java
+++ src/Ponyville/branchOne.java
@@ -1,10 +1,15 @@
 package Ponyville;
 
 public class branchOne {
-
-	public branchOne() {
+	public int featureID;
+	public String feature_description;
+	
+	public branchOne(int id, String desc) {
 		// TODO Auto-generated constructor stub
 		System.out.println("great,a new branch!");
+		//Some new features below:
+		this.featureID = id;
+		this.feature_description = desc;
 	}
 
 }
